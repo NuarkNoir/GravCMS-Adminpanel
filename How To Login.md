23d112a7bf4fb5665ba3cce0495313f9593d6e00
Andrew <mrcluster@yandex.ru>
2017-03-05 22:52:10 +0700

    Commit #1
    Init Commit

---
+++ How To Login.md
@@ -0,0 +1,6 @@
+How to login:
+data[username] = Your login
+data[password] = Your password
+task = login //Constant!!
+redirect = http://nuarknoir.h1n.ru/admin //Constant!! My case!!
+admin-nonce = get from form
---
+++ app/src/androidTest/java/com/nuark/mobile/gravcms/adminpanel/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.nuark.mobile.gravcms.adminpanel;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.nuark.mobile.gravcms.adminpanel", appContext.getPackageName());
+    }
+}
